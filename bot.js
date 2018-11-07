638dc345148cd4f057b48d2a563f7193b42bce41
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-11-07 02:58:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,11 +7,16 @@ client.on('ready', () => {
 
 
 client.on('ready', () => {
-var server = "342590736090529794"; // ايدي السررفر
-var channel = "508379742072995851";//ايدي الروم
+var server = "509529711093874708"; // ايدي السررفر
+var channel = "509529711093874711";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , **')
     },1);
+  
+client.on('ready', () => {
+var server = "509529711093874708"; // ايدي السررفر
+var channel = "509529711093874711";//ايدي الروم
+    setInterval(()=>{
  client.guilds.get(server).channels.get(channel).send('**3kkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , 3kkkkk , **')
     },1);
 })
