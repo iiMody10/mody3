e6e26001d379a1bb646a2d8486843e86c5635b8f
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-11-09 00:34:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,33 +14,6 @@ var channel = "509529711093874711";//ايدي الروم
     },1);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const Discord = require("discord.js");
-const client = new Discord.Client();
-var prefix = "+";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Cyhper Bot Script By : DREAM`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Spam mody`,"http://twitch.tv/Dream")
-client.user.setStatus("dnd")
-});
   
   
 
