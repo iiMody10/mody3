75bf543008c2205bb67c24961f9ea874303a7029
iiMody10 <44102988+iiMody10@users.noreply.github.com>
2018-11-09 00:28:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,35 @@ var channel = "509529711093874711";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , 5kkkkk , **')
     },1);
-})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Spam mody`,"http://twitch.tv/Dream")
+client.user.setStatus("dnd")
+});
   
   
 
